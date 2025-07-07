bb70c8f68aa4026baf02c1f1df6e6f3e3b4248e5
andrea <andrea.gitcommit@gmail.com>
2025-07-07 14:50:51 -0400

    Building Form and Event Logic

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,12 +7,12 @@ const api = new PokeApi();
 async function fetchPokemon() {
   try {
     const response = await api.getPokemon();
-    console.log("RESP DATA:", response);
+    // console.log("RESP DATA:", response);
     displayPokemon(response);
 
     // render function for Pokemon
   } catch (error) {
-    console.error("API ERROR: No Pokemon Found!");
+    console.error("FETCHPOKEMON() ERROR: No Pokemon Found!");
   }
 }
 await fetchPokemon(); // await means not called on page load? what's await doing?
@@ -33,8 +33,7 @@ function displayPokemon(data) {
   console.log("DATA:", data);
 
   // create page components
-  const hero = document.querySelector("#hero");
-  // const whosThat = document.createElement("div"); // V2
+  const hero = document.querySelector("#whosThat");
 
   // CARD
   const whosThatCard = document.createElement("div");
@@ -45,51 +44,68 @@ function displayPokemon(data) {
   const whosThatTitle = document.createElement("h3");
   whosThatTitle.classList.add("hero__poke-reveal");
   whosThatTitle.classList.add("#reveal");
-  whosThatTitle.textContent = `${data.name}`;
-  // whosThatTitle.textContent = "Who is it?";
-  // logic for correct guess to flip content to Pokemon name
+  whosThatTitle.textContent = "Who is it?";
+
+  if (whosThatTitle.querySelector("hero__poke-reveal--true")) {
+    // in event listener! - logic for correct guess to flip content to Pokemon name
+    // check if event listener is working!
+    whosThatTitle.textContent = `${data.name}`; // WORKS!
+  }
 
   // DYNAMIC IMAGE
   const whosThatPoke = document.createElement("img");
   whosThatPoke.classList.add("hero__poke-img");
   whosThatPoke.classList.add("#poke-img");
+  whosThatTitle.querySelector("hero__poke-img"); // need CSS to blackout the sprite!
   whosThatPoke.src = `${data.sprites.front_default}`;
-  // whosThatPoke.innerText = {response.data.sprites.front_gray}
+
+  if (whosThatTitle.querySelector("hero__poke-img--true")) {
+    // in event listener! - logic for correct guess to remove dark overlay from sprite
+    // check if event listener is working!
+    whosThatPoke.src = `${data.sprites.front_default}`; // WORKS!
+  }
+  // probably grayscale and not blacked out
+  // whosThatPoke.src = `${
+  //   data.data.sprites.versions.generation - i.yellow.front_gray
+  // }`; // escape character needed but failing
 
   // PUT CARD ALL TOGETHER
   whosThatCard.appendChild(whosThatTitle);
   whosThatCard.appendChild(whosThatPoke);
   // ADD TO HERO SECTION
-  // whosThat.appendChild(whosThatCard); // V2
   hero.appendChild(whosThatCard);
-
-  // return whosThat; // V2
   return hero;
 }
 
-// V2
-// function renderPage(poke) {
-//   const hero = document.querySelector("#hero");
-//   hero.appendChild(whosThat);
-// }
-
 // ---------- SUBMIT GUESS EVENT LISTENER ------------
-// need to create guessForm element
-// guessForm.addEventListener("submit", async (e) => {
-//   e.preventDefault();
+// link event listener to guess form
 
-//   const guess = {
-//     name: e.target.guessPoke.value
-//   };
+window.addEventListener("DOMContentLoaded", (data) => {
+  console.log("In event listener");
 
-//   // try {
-//   // need a local api to post guesses to, and which compares against PokeApi to return match or not
-//   // need function to make POST request
-//   //     const response = await.api.postGuess(guess);
-//   //     // fxn to confirm guess or prompt retry
+  // grab guess form element
+  const guessForm = document.querySelector("#guess");
 
-//   // } catch (error) {
-//   //     console.error('Failed to submit guess, please try again.');
-//   // }
-//   guessForm.reset();
-// });
+  guessForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const guess = {
+      name: e.target.guessPoke.value
+    };
+
+    if (guess === data.name) {
+      whosThatTitle.classList.add("hero__poke-reveal--true"); // show pokemon name
+      whosThatPoke.classList.add("hero__poke-img--true"); // remove blackout on sprite
+    }
+
+    // need a local api to post guesses to, and which compares against PokeApi to return match or not
+    // try {
+    //   // need function to make POST request
+    //   // const response = await.api.postGuess(guess);
+    //   // fxn to confirm guess or prompt retry
+    // } catch (error) {
+    //   console.error("Failed to submit guess, please try again.");
+    // }
+    guessForm.reset();
+  });
+});
